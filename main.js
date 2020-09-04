85a3054c06402cadd0accf3da3e881a90b736834
Micky <miguelksangel@gmail.com>
2020-09-04 15:46:54 -0600

    Auto-generated commit

--- main.js
+++ main.js
@@ -405,7 +405,7 @@ AppComponent.ɵcmp = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineCompo
                 styleUrls: ['./app.component.css']
             }]
     }], null, null); })();
-console.log("py1");
+console.log("Prueba f");
 
 
 /***/ }),
